c1d4616747ac502bba84ab0146c1bad73179b8c2
Vivek Dubey <dubeyv@tabcorp.com.au>
2019-01-02 16:25:45 +1100

    Fixed mfa verify route

--- routes/mfa.js
+++ routes/mfa.js
@@ -1,24 +1,18 @@
 const mfaRouter = require('express').Router();
 const request = require('request');
 const mfa = require('../lib/2fa');
-const secret = mfa.secret()
 
 mfaRouter.get('/',
 require('connect-ensure-login').ensureLoggedIn('/login'),
 function (req, res) {
-  let url = mfa.otpUrl(secret.base32,req.user.username)
-  console.log( url)
+  req.user.secret = mfa.secret().base32
+  let url = mfa.otpUrl(req.user.secret,req.user.username)
+  console.log(url)
   res.render('mfa', {otpUrl: url});
 });
 
 mfaRouter.post('/verify', function (req, res) {
-  let token = req.body.token;
-  let tokenVerified = mfa.verifyToken(token, secret.base32)
-  let message = {verified: false}
-  if(tokenVerified){
-    message.verified = true
-  }
-  res.render('verify', message);
+  res.render('verify', {verified: mfa.verifyToken(req.body.token, req.user.secret)})
 });
 
 module.exports = mfaRouter;
