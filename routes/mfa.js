13a1afd555f9e7b0fa47b0b692137913ef77979a
Vivek Dubey <dubeyv@tabcorp.com.au>
2019-01-02 16:49:49 +1100

    Updated session name for secret

--- routes/mfa.js
+++ routes/mfa.js
@@ -5,14 +5,14 @@ const mfa = require('../lib/2fa');
 mfaRouter.get('/',
 require('connect-ensure-login').ensureLoggedIn('/login'),
 function (req, res) {
-  req.user.secret = mfa.secret().base32
-  let url = mfa.otpUrl(req.user.secret,req.user.username)
+  req.session.mfa = {secret: mfa.secret().base32}
+  let url = mfa.otpUrl(req.session.mfa.secret,req.user.username)
   console.log(url)
   res.render('mfa', {otpUrl: url});
 });
 
 mfaRouter.post('/verify', function (req, res) {
-  res.render('verify', {verified: mfa.verifyToken(req.body.token, req.user.secret)})
+  res.render('verify', {verified: mfa.verifyToken(req.body.token, req.session.mfa.secret)})
 });
 
 module.exports = mfaRouter;
